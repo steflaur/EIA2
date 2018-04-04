d29b99f42fbc42919d5dad422926f8ec96bb21d6
laura <laura@lauras-mbp.fritz.box>
2018-04-04 15:03:12 +0200

    A00 upload

---
+++ A00/A00.js
@@ -0,0 +1,22 @@
+/*
+Aufgabe 00: Miniseite
+Name: Laura Vogt
+Matrikel: 256056
+Datum: 22.Mrz18
+
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+function Greeter() {
+    let name = prompt("Wer klopft?", "knockknock");
+    let text = document.getElementById("txt").innerHTML;
+    if (name == null || name == "knockknock") {
+        text = "Schade!";
+    } //if close
+    else {
+        text = "Hallo " + name + "! Wie geht es dir?";
+    } //else close
+    document.getElementById("txt").innerHTML = text;
+} //funktion close
+//# sourceMappingURL=A00.js.map
---
+++ A00/A00.ts
@@ -0,0 +1,28 @@
+/* 
+Aufgabe 00: Miniseite
+Name: Laura Vogt
+Matrikel: 256056
+Datum: 22.Mrz18
+
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert. 
+*/
+
+
+
+    function Greeter(): void {
+        let name: string = prompt("Wer klopft?", "knockknock");
+        let text: string = document.getElementById("txt").innerHTML
+        
+        if (name == null || name == "knockknock") {
+            text = "Schade!";
+        }//if close
+        
+            else {
+               text = "Hallo " + name + "! Wie geht es dir?";
+            }//else close
+        
+        document.getElementById("txt").innerHTML= text;
+    }//funktion close
+    
