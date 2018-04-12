42a788ba395aae458bc4ce2a1aba608f24213caa
laura <laura@lauras-mbp.eduroam.hs-furtwangen.de>
2018-04-12 14:57:24 +0200

    new

--- A02/scripts/main.ts
+++ A02/scripts/main.ts
@@ -12,38 +12,37 @@ namespace memory {
 
 
 //global lets
-let pairsSumInput : number;
-let playersSumInput : number;
+
+
     
 
 function init(): void {
     
-    let pairsSum : string = prompt("Wie viele Kartenpaare (min5|max10)")
-    pairsSumInput = parseInt(pairsSum)
+    let pairs : string = prompt("Wie viele Kartenpaare (min5|max10)")
+    let pairsSum : number = parseInt(pairs)
     
-    if (isNaN(pairsSumInput) || pairsSumInput <= 5 || pairsSumInput >=10) {
+    if (isNaN(pairsSum) || pairsSum < 5 || pairsSum > 10) {
         alert("FALSCH")
         init();    
     }
     
         else {
         
-            let playersSum : string = prompt("Wie viele Spieler (min2|max4)")
-            playersSumInput = parseInt(playersSum)
+            let player : string = prompt("Wie viele Spieler (min1|max4)")
+            let playerSum : number = parseInt(player)
         
-            if (isNaN(playersSumInput) || playersSumInput >= 2 || playersSumInput <=4) {
+            if (isNaN(playerSum) || playerSum < 1 || playerSum > 4) {
                 alert("FALSCH")
                 init();    
             }
             
             else {
-                createBrett(pairsSumInput,playersSumInput);
+                createGame(pairsSum,playerSum);
                 }
-        
         }
-}
+}//init close
 
-function createBrett(pairs: number, player: number) : void {
+function createGame(_pairs: number, _player: number) : void {
     
     //header
     let head : HTMLElement = document.createElement("header")
@@ -54,86 +53,46 @@ function createBrett(pairs: number, player: number) : void {
     h.innerText = "Memory";
     head.appendChild(h);
     
+    
+    createMainboard();
+    
+    createAside();
+    
+    createFooter();
+    
+}//function game close
+
+ 
+ 
+function createMainboard () : void {
+
     //main
     let mainboard : HTMLElement = document.createElement("main")
     document.body.appendChild(mainboard);
-    createMainboard();
+    
+}//create Main close
+
+    
+    
+function createAside () : void {
     
     //aside
     let info : HTMLElement = document.createElement("aside")
     document.body.appendChild(info);
     
-    let content : string;
+}//create Aside close 
     
-    for (var i : number = 0; i < playersSumInput; i++) {
-        
-        content = "";
-        content += "<fieldset>";
-        
-        content += "<legend>";
-        content += "Player "+ i++; //damit nicht player 0
-        content += "</legend>";
-        
-        content += "<p>";
-        content += "Score: ";
-        content += "9347503"; //variable...
-        content += "</p>";
-        
-        content += "</fieldset>";
-        
-        }
-    
-    info.innerHTML = content;
-    
-    //footer
-    
-}//function brett
 
     
-    //array inhalte
-    []
-    let divs: NodeListOf<HTMLDivElement> = document.getElementsByTagName("div");
+function createFooter () : void {
     
-    for (let i : number = 0; i > pairsSumInput; i++) {
-       
-        //Variablen machen
-       // let card1: HTMLDivElement = document.createElement("div");
-       // let card2: HTMLDivElement = document.createElement("div");
-       
-        
-       //klasse zuweisen 
-       // card1.class += "Paar" + i 
-       // card2.class += "Paar" + i
-        
-        //Inhalt
-        // card1.innerText += i
-        // card2.innerText += i
-      }  
- 
-function createMainboard () : void {
-  //array
+    //footer
+    let footer : HTMLElement = document.createElement("footer")
+    document.body.appendChild(footer);
     
-    let cardContent : string[] =
-            ["A","B","C","I","E",
-             "F","G","H","I","J"];
+}//create Footer close    
     
-    
-    let cardA: HTMLDivElement;
-    let cardB: HTMLDivElement;
-    let lines : number = 0;
-    
-        for (var i : number = 0; i < pairsSumInput; i++) {
-         let cardA = document.createElement("div");
-         let cardB = document.createElement("div");
-  //      mainboard.appendChild(cardA,cardB);   
-               
-            
-        
-        
-    }
-    
-    
-} 
+   
     
 }//namespace ende        
         
