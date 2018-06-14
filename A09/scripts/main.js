f3f3eedaee89826649b5b1c0d022088a860e8c14
laura <laura@lauras-mbp.fritz.box>
2018-06-14 12:45:07 +0200

    konzept + raw

--- A09/scripts/main.js
+++ A09/scripts/main.js
@@ -16,8 +16,27 @@ var A09_canvas;
         let canvas = document.getElementsByTagName("canvas")[0];
         let ctx = canvas.getContext("2d");
         console.log("CanvasRendering2d active");
-        ctx.fillStyle = "blue";
-        ctx.fillRect(0, 0, 1000, 600);
+        ctx.strokeStyle = "black";
+        //Wasser
+        var gradient = ctx.createRadialGradient(400, 520, 10, 400, 400, 400);
+        gradient.addColorStop(0, 'white');
+        gradient.addColorStop(1, 'lightblue');
+        ctx.fillStyle = gradient;
+        ctx.fillRect(0, 0, 800, 600);
+        ctx.strokeRect(0, 0, 800, 600);
+        //Boden
+        ctx.beginPath();
+        ctx.moveTo(0, 400);
+        ctx.quadraticCurveTo(100, 340, 240, 440);
+        ctx.quadraticCurveTo(400, 540, 600, 460);
+        ctx.quadraticCurveTo(700, 420, 800, 460);
+        ctx.lineTo(800, 600);
+        ctx.lineTo(0, 600);
+        ctx.lineTo(0, 400);
+        ctx.stroke();
+        ctx.fillStyle = "#dbd1b3";
+        ctx.fill();
+        ctx.closePath();
     } //close init
 })(A09_canvas || (A09_canvas = {})); //close namespace
 //# sourceMappingURL=main.js.map
--- A09/scripts/main.ts
+++ A09/scripts/main.ts
@@ -20,20 +20,45 @@ namespace A09_canvas {
         let ctx: CanvasRenderingContext2D = canvas.getContext("2d");
         console.log("CanvasRendering2d active");
 
+
+        ctx.strokeStyle = "black";
+        //Wasser
+
+
+        var gradient = ctx.createRadialGradient(400, 520, 10, 400, 400, 400);
+        gradient.addColorStop(0, 'white');
+        gradient.addColorStop(1, 'lightblue');
+        ctx.fillStyle = gradient;
+        ctx.fillRect(0, 0, 800, 600);
+
+        ctx.strokeRect(0, 0, 800, 600);
         
-      
-        
+        //Boden
+        ctx.beginPath();
+        ctx.moveTo(0, 400);
+        ctx.quadraticCurveTo(100, 340, 240, 440);
+        ctx.quadraticCurveTo(400, 540, 600, 460);
+        ctx.quadraticCurveTo(700, 420, 800, 460);
+        ctx.lineTo(800, 600);
+        ctx.lineTo(0, 600);
+        ctx.lineTo(0, 400);
+        ctx.stroke();
+        ctx.fillStyle= "#dbd1b3";
+        ctx.fill();
+        ctx.closePath();
+
+
     }//close init
-    
-    
+
+
     //aquarium
     //Hintergrund mit gradient
     //boden
     //fische 1
     //fische 2
     //pflanzen
-    
+
     //schatzkiste
-    
-    
+
+
 }//close namespace
